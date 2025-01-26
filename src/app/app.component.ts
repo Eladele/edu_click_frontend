ae29410edcb99dd77c1053f011dd061cb767917e
abdallahilili <125148924+abdallahilili@users.noreply.github.com>
2025-01-26 23:59:46 +0000

    fix les probleme de bootstrap et assets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { IndexComponent } from "./features/index/index.component";
+import { HeaderComponent } from "./shared/components/header/header.component";
 
 @Component({
   selector: 'app-root',
--- src/app/features/index/index.component.ts
+++ src/app/features/index/index.component.ts
@@ -1,28 +1,40 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import {FormsModule} from '@angular/forms'
 import { RouterLink } from '@angular/router';
+import { HeaderComponent } from '../../templates/header/header.component';
+interface Professeur {
+  id: number;
+  nom: string;
+  prenom: string;
+  image : string;
+  ville: string;
+  adresse: string;
+  disponibilites: string[];
+  prix: number;
+  lieuxEnseignement: string[];
+  biographie: string;
+}
 
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
-  styleUrls: ['./index.component.scss'],
+  styleUrls: ['./index.component.css'],
   standalone: true,
-  imports: [FormsModule, RouterLink] 
- 
+  imports: [FormsModule, RouterLink, CommonModule, HeaderComponent],
 })
 export class IndexComponent {
   searchQuery: string = '';
-  professeurs: any[] = [];
+  professeurs: Professeur[] = []; // Initialize as an empty array
 
-  // Simulation d'une recherche
   onSearch() {
-    // Simuler une recherche ou appeler une API pour récupérer les professeurs
     this.professeurs = this.searchQuery
       ? [
           {
             id: 1,
             nom: 'Doe',
             prenom: 'John',
+            image: 'assets/images/professeur1.jpg',
             ville: 'Nouakchott',
             adresse: 'Centre ville',
             disponibilites: ['Lundi', 'Mardi'],
@@ -31,6 +43,6 @@ export class IndexComponent {
             biographie: 'Professeur expérimenté en Mathématiques.',
           },
         ]
-      : [];
+      : []; // Ensure it's an empty array if no search query
   }
 }
